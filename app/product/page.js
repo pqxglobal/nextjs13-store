b578b152af9bd040970d1bb474eea6c32206f2d5
pqxglobal <l.r.slegt@gmail.com>
2023-05-05 13:11:22 +0200

    🐛 Commented out

--- app/product/page.js
+++ app/product/page.js
@@ -10,9 +10,9 @@ export default function ProductPage(props) {
 
   console.log(productInfo);
 
-  if (!product?.name && typeof window !== 'undefined') {
-    window.location.href = "/";
-  }
+  // if (!product?.name) {
+  //   window.location.href = "/";
+  // }
 
   function handleAddToCart() {
     const newItem = {
